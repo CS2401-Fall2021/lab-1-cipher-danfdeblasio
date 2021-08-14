fdf6ea998d74a3059b2faae8c72a17640dd3785d
Dan DeBlasio <dan@dandeblasio.com>
2021-08-14 08:28:01 -0600

    simple answer?

--- Cipher.java
+++ Cipher.java
@@ -5,12 +5,28 @@ public class Cipher{
   }
      // TODO: write a function that implements a cesear cipher
   public String encode(String s){
-    return s;
+    String rtn = "";
+    for(int i=0; i<s.length(); i++){
+      if(s.charAt(i) != ' '){
+        rtn += (char)((int)s.charAt(i)+shift);
+      }else{
+        rtn += ' ';
+      }
+    }
+    return rtn;
   }
 
   // Followup Problem1: You are at headquarters, and receive an encrypted message. 
   // You know the number by which it has been shifted. Write a method to decrypt it.
   public String decode(String s){
-    return s;
+    String rtn = "";
+    for(int i=0; i<s.length(); i++){
+      if(s.charAt(i) != ' '){
+        rtn += (char)((int)s.charAt(i)-shift);
+      }else{
+        rtn += ' ';
+      }
+    }
+    return rtn;
   }
 }
--- Main.java
+++ Main.java
@@ -2,12 +2,17 @@ class Main {
   public static void main(String[] args) {
      runTests();
   }
-  public void runTests(){
+  public static void runTests(){
     // Here is an example test case
    String messageToEncode="here is a top secret message";
    String sampleEncoded="khuh lv d wrs vhfuhw phvvdjh";
    int shift =3;
    Cipher cipher= new Cipher(shift);
+
+   // Can we change this to create a temp string on encode
+   // also we're avoiding objects, so maybe we can make it a static method within this class? 
+   // i.e. encode(String in, int shift) and decode(String in, int shift)
+   System.out.println(cipher.encode(messageToEncode));
    System.out.println("Did it work?"+cipher.encode(messageToEncode).equals(sampleEncoded));
 
   // TODO: implement 3 more test cases, like the one above using inputs that handle the edge cases you've identified.
